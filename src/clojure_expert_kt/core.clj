2b720d9e88dd69d0910e81d54d3459fcf3a66711
k.tepin <aikoridiel@gmail.com>
2021-10-15 23:32:41 +0300

    Two sessions logic

--- src/clojure_expert_kt/core.clj
+++ src/clojure_expert_kt/core.clj
@@ -8,6 +8,7 @@
 (defn examine-conditions [param])
 (defn insert-knowledge-base [param])
 (defn ask-user [param])
+(defn examine-item-with-conditions [param1 param2])
 
 ;;Свойства 
 (defrecord Condition [number descr])
@@ -16,19 +17,16 @@
 ;;Ответы пользователя
 (defrecord Answer [condition iscorrect])
 
-(defrule consult
+(defrule consult-rule
   "Производит экспертный анализ по базе знаний"
   [Weapon (= ?name name) (= ?conditions conditions)] ;;перебор фактов как в прологе с механизмом связывания перменных
   =>
-  (examine-conditions ?conditions))
+  (examine-item-with-conditions ?name ?conditions))
 
-(defquery find-answer
+(defquery find-answer-query
   "Поиск факта ответа с указанным свойством"
   []
-  [?condition <- Answer (= ?cond condition) (= ?state iscorrect)])
-
-;;в этот момент будет создана сессия. Обязательно объявить все rules и queries перед 'nb
-(def my-session nil)
+  [Answer(= ?test condition)])
 
 (defn insert-knowledge-base
   "База знаний"
@@ -37,9 +35,6 @@
       (insert (->Condition 2 "Гладкий ствол"))
       (insert (->Condition 3 "Дробовик"))
       (insert (->Condition 4 "Карабин"))
-
-      (insert (->Answer 2 :true))
-      (insert (->Answer 4 :true))
 
       (insert (->Weapon "Тигр 7.62" '(2 4)))
       (insert (->Weapon "Saiga-12 Ижмаш" '(2 3)))
@@ -52,34 +47,34 @@
   (println (str condition " ? [y/n]"))
   (= "y" (read-line)))
 
-(defn print-ses
-  [session]
-  (println (inspect session))
-  session)
+(defn examine-item-with-conditions
+  "Test"
+  [item cond-list]
+  (let [session (mk-session [find-answer-query] :cache false)]
+    (-> session
+        (insert (->Answer 2 :true))
+        (insert (->Answer 4 :true))
+        (fire-rules)
+        (examine-conditions cond-list)) 
+    ))
 
 (defn examine-conditions
   "Рекурсиваня проверка списка свойств"
-  [condition-list]
+  [session condition-list]
   ;;(ask-user (first condition-list))
   (println (str (first condition-list)))
+  (println (query session find-answer-query))
   ;;(print-ses my-session)
-   (println (query my-session find-answer))
   (if (empty? (rest condition-list))
     true
-    (examine-conditions (next condition-list)))) ;;рекурсия
-
-(defn save-session
-  [session]
-  (def my-session session)
-  session)
+    (examine-conditions session (next condition-list)))) ;;рекурсия
 
 (defn -main
   "Main entery"
   [& args]
   
-  (-> (mk-session [consult find-answer] :cache false)
+  (-> (mk-session [consult-rule] :cache false)
       (insert-knowledge-base)
-      (save-session)
       (fire-rules))
   ;; (println (str (first #{1 2})))
   nil)
